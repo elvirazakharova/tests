995dd69cf279ae4f3b177c4f8f78d0d5b76e7b96
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-10-31 14:19:18 +0300

    downcase для русского языка; nil
    + downcase метод для русских букв
    + обработка nil

--- 1.rb
+++ 1.rb
@@ -1,29 +1,41 @@
-#downcase не работает с русскими буквами.
-#gem install activesupport
-
-def palinom?(arg1)
-  rez = false
-  arg2 = "" #здесь будет лежать "чистая" строка
-  #тут привожу варианты, как это можно сделать
-  #arg2 = (arg1.tr(" _ -,.?", '')).downcase  # вариант 1. можно заменить все символы пунктуации на пустоту
-  #arg2 = (arg1.tr("^a-zA-Zа-яА-Я", '')).downcase #вариант 2. можно заменить все, что не буквы (русские или английские) на пустоту
-  #arg1.downcase.scan(/["а-яА-Яa-zA-Z"]/).each { |aa| arg2<<aa } #вариант 3. можно сделать такие странные действия \w не подойдет совсем
-  #arg1.downcase.scan(/["а-яА-Яa-zA-Z"]/).each { |aa| arg2.concat(aa) } #вариант 4. то же самое, что и 3
-  #arg1.downcase.each_char { |aa| arg2.concat(aa) if /["а-яА-Яa-zA-Z"]/ =~ aa } #вариант 5. каждую букву пишем в новую строку.
-  arg1.downcase.each_char { |aa| arg2.concat(aa) if /["а-яА-Яa-zA-Z"]/ =~ aa } #вариант 5. каждую букву пишем в новую строку.
-  puts "Чистая строка: #{arg2}"
-  if arg2 == arg2.reverse && arg2 != ""
-    rez = true
-  end
-  return rez
+#downcase дополненный для русского языка
+def downcase_r (arg)
+    alpha1 = (('А'..'Я').to_a )<<'Ё'
+    alpha2 = (('а'..'я').to_a )<<'ё'
+    for i in 0..(arg.length-1) do
+        #puts "i= #{i}"
+        if  alpha1.index(arg[i])
+            arg[i] = alpha2[alpha1.index(arg[i])]
+        end
+    end
+    #arg1 = arg.join('')
+    arg.downcase
 end
-
+#функция палином
+def palinom?(arg1)
+    if arg1 
+      rez = false
+      arg2 = "" #здесь будет лежать "чистая" строка
+      arg1.each_char { |aa| arg2.concat(aa) if /["а-яА-Яa-zA-ZёЁ"]/ =~ aa } # каждую букву пишем в новую строку.
+      arg2 = downcase_r(arg2)
+      puts "Чистая строка: #{arg2}"
+      if arg2 == arg2.reverse && arg2 != ""
+        rez = true
+      end
+    else
+       rez = false
+  end
+  rez #return убрала. 
+end
+#куча примеров, в том числе и пустая строка
 a = "А роза упала на лапу азора?, ---------12324235346" #  "Азоза"
 puts palinom?(a)
-b = ""
+b = nil
 puts palinom?(b)
 c = "DSDS     (ssD) s     d!"
 puts palinom?(c)
 d = "Ни разу не палином"
 puts palinom?(d)
-STDIN.getc
+d = "АБЫар fdfff D        f--РАыба"
+puts palinom?(d)
+#STDIN.getc
