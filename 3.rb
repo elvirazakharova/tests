1e8e74d5eb7a0feb8ac83f139628679401ede83c
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-01 13:55:40 +0300

    +исключения +функция
    Добавлены исключения

--- 3.rb
+++ 3.rb
@@ -1,17 +1,74 @@
-a = [["Amanda", "S"],["Peter", "R"]]
-puts "#{a}"
-puts "P - бумага"
-puts "R - камень"
-puts "S - ножницы"
-    case 
-    when a.length != 2
-      puts "WrongNumberOfPlayersError"
-#    when ((a[0][1].slice(/[^"SPR"]/))||(a[1][1].slice(/[^"SPR"]/))) #это не работает так, как надо
-#      puts "NoSuchStrategyError"
-    when ((a[0][1] == "S")&&(a[1][1]=="P"))||((a[0][1] == "R")&&(a[1][1]=="S"))||((a[0][1] == "P")&&(a[1][1]=="R"))||(a[0][1] == a[1][1])
-      puts "Winner = #{a[0]}"
-    when ((a[0][1] == "P")&&(a[1][1]=="S"))||((a[0][1] == "R")&&(a[1][1]=="P"))||((a[0][1] == "S")&&(a[1][1]=="R"))
-      puts "Winner = #{a[1]}"
+=begin
+Мы будем разрабатывать методы для программы Камень-Ножницы-Бумага.
+Метод rps_game_winner должен принимать на вход массив примерно следующей структуры
+[ ["Armando", "P"], ["Dave", "S"] ], где P - бумага, S - ножницы, R - камень, и функционировать
+следующим образом:
+• если количество игроков больше 2 необходимо вызывать исключение WrongNumberOfPlayersError;
+• если ход игроков отличается от ‘R’, ‘P’ или ‘S’ необходимо вызывать исключение
+NoSuchStrategyError;
+• в иных случаях необходимо вернуть имя и ход победителя, если оба игрока походили одинаково -
+выигрывает первый игрок.
+=end
+puts "R - камень
+S - ножницы
+P - бумага"  
+def RSP (arg)
+    begin      
+             puts "#{arg}"
+    raise "WrongNumberOfPlayersError" if arg.length != 2
+            h = { "R" => 1, "S" => 2, "P" => 3}
+            a = h[arg[0][1]]
+            b = h[arg[1][1]]
+            if a == b 
+                puts "Одинаковые значения"
+                rez =  arg[0]
+            else
+                c = (a-b).abs #"расстояние"
+                if ( c == ((a-b)*(-1)**(c+1))) #лаконично и понятно в данном случае противоположные вещи
+                    puts "Второй выиграл"
+                    rez = arg[1]
+                else
+                    puts "Первый выиграл"
+                    rez = arg[0]
+                end
+            end
+            return rez
+     rescue RuntimeError => error
+     puts error.inspect
+     rescue
+     puts "NoSuchStrategyError"
+     end
+end
+aa = nil
+puts RSP(aa)
+aa = [ ["Armando", "S"]]  
+puts RSP(aa)
+aa = [ ["Armando", "S"], ["Dave", "S"], ["Dave", "S"] ]  
+puts RSP(aa)
+aa = [ ["Armando", "SS"], ["Dave", "P"] ] 
+puts RSP(aa)
+aa = [ ["Armando", "R"], ["Dave", "P"] ] 
+puts RSP(aa)
+aa = [ ["Armando", "P"], ["Dave", "R"] ] 
+puts RSP(aa)
+aa = [ ["Armando", "S"], ["Dave", "R"] ] 
+puts RSP(aa)
+aa = [ ["Armando", "R"], ["Dave", "S"] ] 
+puts RSP(aa)
+=begin проще, понятнее, но длиннее
+def aaaaa (a, b)
+    if (a-b).abs==1 
+        if a-b<0
+            return a
+        else 
+            return b
+        end
     else
-      puts "NoSuchStrategyError"
-    end
+        if a-b<0
+            return b
+        else 
+            return a
+        end
+    end        
+end
+=end
