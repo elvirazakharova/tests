9453204e99b7ce594e97a2068a96f70397b0aff6
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-07 14:48:03 +0300

    Краткость +1

--- 3.rb
+++ 3.rb
@@ -10,52 +10,29 @@ NoSuchStrategyError;
 выигрывает первый игрок.
 =end
 @h = { "R" => 1, "S" => 2, "P" => 3}
-puts "R - камень
-S - ножницы
-P - бумага"  
 class NoSuchStrategyError<Exception
+    def initialize(msg="No such strategy error") ;  super;  end
 end
 class WrongNumberOfPlayersError<Exception
     def message ; "Не верно количество игроков"; end
 end
 def RSP (arg)
     begin      
-            puts "#{arg}"
-            raise NoSuchStrategyError if arg == nil
+            raise NoSuchStrategyError, "nil" if arg == nil 
             raise WrongNumberOfPlayersError if arg.length != 2
-            raise NoSuchStrategyError if (@h[arg[0][1]].nil?)||(@h[arg[1][1]].nil?) #в хэше нет таких значений
-            a = @h[arg[0][1]]
-            b = @h[arg[1][1]]
-            if a == b 
-                rez =  arg[0]
+            raise NoSuchStrategyError if (@h[arg[0][1]].nil?)||(@h[arg[1][1]].nil?)
+            a, b = @h[arg[0][1]], @h[arg[1][1]]
+            if a == b
+                arg[0]
             else
-                c = (a-b).abs #"расстояние"
-                if ( c == ((a-b)*(-1)**(c+1))) #лаконично и понятно в данном случае противоположные вещи
-                    rez = arg[1]
-                else
-                    rez = arg[0]
-                end
+                (a-b).abs == ((a-b)*(-1)**((a-b).abs + 1)) ? arg[1] : arg[0]
             end
-            return rez
      rescue WrongNumberOfPlayersError => e
      puts e.message
      rescue NoSuchStrategyError => e
      puts e.message
      end
 end
-aa = nil
-puts RSP(aa)
-aa = [ ["Armando", "S"]]  
-puts RSP(aa)
-aa = [ ["Armando", "S"], ["Dave", "S"], ["Dave", "S"] ]  
-puts RSP(aa)
-aa = [ ["Armando", "S"], ["Dave", "A"] ] 
-puts RSP(aa)
-aa = [ ["Armando", "R"], ["Dave", "P"] ] 
-puts RSP(aa)
-aa = [ ["Armando", "P"], ["Dave", "R"] ] 
-puts RSP(aa)
+
 aa = [ ["Armando", "S"], ["Dave", "R"] ] 
-puts RSP(aa)
-aa = [ ["Armando", "R"], ["Dave", "S"] ] 
 puts RSP(aa)
