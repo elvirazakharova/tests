07aece118e5c061fa9bf8d06454a5c9780b23799
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-07 15:15:38 +0300

    Как правильно расширить метода класса String?

--- 1.rb
+++ 1.rb
@@ -8,23 +8,28 @@ palindrome?("Abracadabra") # => false (nil is also ok)
 
 '
 class String
-    def clear_downcase_UTF8
+    alias d downcase
+    def downcase
        temp = ""
        self.each_char do |a| 
            case a.ord 
-           when (1040..1071), (65..90) #А-Я #A-Z
+           when (1040..1071) #, (65..90) #А-Я #A-Z
                 temp << (a.ord + 32).chr(Encoding::UTF_8) 
            when 1025 #Ё
                 temp << 1105.chr(Encoding::UTF_8) 
-           when (1040+32..1071+32), (65+32..90+32),  1105
-               temp << a
+           else #when (1040+32..1071+32), (65+32..90+32),  1105
+               temp << a.d
            end 
         end
         temp
     end
 end
 def palindrome?(arg)
-    arg ? arg.clear_downcase_UTF8 == arg.clear_downcase_UTF8.reverse : false
+        temp = ""
+        arg.downcase.each_char {|pp|  temp = temp+pp if pp =~ /["а-яА-Яa-zA-ZёЁ"]/} if arg
+        puts temp
+        arg ?  temp == temp.reverse   : false
+    
 end
 
 
