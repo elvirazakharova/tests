f8c043e00cb038e7cbe0ca6117bfdde4bcfe99e8
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-08 10:31:12 +0300

    +attr_accessor + значения по умолчанию - to_s

--- 5.rb
+++ 5.rb
@@ -1,30 +1,22 @@
-STDOUT << 'Реализуйте класс Dessert c геттерами и сеттерами для полей класса name и calories, конструктором,
-принимающим на вход name и calories, а также двумя методами healthy? (возвращает true при
-условии калорийности десерта менее 200) и delicious? (возвращает true для всех десертов).
-
-'
-
 class Dessert
-    def initialize(name, calories)
+    def initialize(name="", calories=0)
         @name, @calories = name, calories
     end
-    def to_s
-      "(#@name, #@calories)"
-    end
-    def name; @name; end
-    def name=(value); @name = value; end    
-    def calories=(value); @calories = value; end
+    attr_accessor :name, :calories
     def healthy?
         calories <200 ? true : false
     end
-    def delicious?
+    def delicios?
         true
     end
 end
-a = Dessert.new("Apple", 76)
+a = Dessert.new()
+b = Dessert.new("Apple", 76)
 puts a.name
-a.calories = 45
+a.calories = 2245
 a.name = "Pinapple"
 puts a.class
-puts a.is_a? Dessert
-puts a
+puts a.healthy?
+puts a.delicios?
+puts a.inspect
+puts b.inspect
