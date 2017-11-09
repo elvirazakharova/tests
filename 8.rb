bb09ca78e690ade4a0cc9b25f65ec5e30af7a309
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-09 15:13:26 +0300

    +class_eval

--- 8.rb
+++ 8.rb
@@ -11,41 +11,25 @@ f.bar_history # => [nil, 3, :wowzo, 'boo!']
 
 "
 
-
-def attr_accessor_with_history (bar)
-    def bar
-        @bar
+class Module
+    def attr_accessor_with_history(bar)
+         class_eval " 
+                        @@#{bar}_hystory = [nil] 
+                        def #{bar}; @#{bar}; end
+                        def #{bar}=(value)
+                            @#{bar} = value
+                            @@#{bar}_hystory << value
+                        end
+                        def #{bar}_hystory;  @@#{bar}_hystory; end
+                    "
     end
-    def bar=(value)
-        @bar = value
-        @history ? @history.push(value) : @history = [nil]
-    end
-    def history
-        @history ? "#{@history}" : "[nil]"
-    end
-    puts b = "#{bar}_history"
-    alias bar_history history
 end
-
-class Foo
+class Foo       
     attr_accessor_with_history :bar
-    def to_s
-        "class #{self.class}, \"#{self.bar}\""
-    end
 end
 
-
-f = Foo.new # => #<Foo:0x127e678>
-puts f.bar_history 
-f.bar = 3 # => 3
-f.bar = :wowzo # => :wowzo
-f.bar = 'boo!' # => 'boo!'
-puts f.bar
-puts f
-puts f.bar_history # => [nil, 3, :wowzo, 'boo!']
-f.bar = 4324
+f = Foo.new
 f.bar = 3
-f.bar = "dsfsd"
-f.bar = nil
-f.bar = "sds"
-puts f.bar_history
+f.bar = :wowzo
+f.bar = 'boo!'
+puts "#{f.bar_hystory}"
