c68b8a7ca7a24accd248032730c4dd24215f5967
Elvira Zakharova <32979831+elvirazakharova@users.noreply.github.com>
2017-11-10 15:08:37 +0300

    :

--- 7.rb
+++ 7.rb
@@ -1,29 +1,25 @@
-STDOUT << 'Сделайте возможным выполнять следующий код:
-5.dollars.in(:euros)
-10.euros.in(:rubles)
-Вы должны поддерживать следующие валюты: dollars, euros и rubles, курс обмена установите
-следующий: $1 = 32,26 руб., 1€ = 43,61 руб.
-Должны подерживаться как единственное, так и множественное именование метода, т.е.
-1.dollar.in(:rubles) и 1.ruble.in(:euro) должны работать.
-
-
-' 
-
-
 module Denezhka
-    RATES = {"euro" => 43.61, "euros" => 43.61, "dollar" =>32.26, "dollars"=>32.26, "ruble"=>1.0, "rubles"=>1.0}
-    def dollar; self*RATES["dollar"]; end
-    def euro; self*RATES["euros"]; end
-    def ruble; self*RATES["ruble"]; end
+    RATES = {:euro => 43.61, :euros => 43.61,
+            :dollar =>32.26, :dollars =>32.26,
+            :ruble =>1.0, :rubles =>1.0}
+    def dollar; self*RATES[:dollar]; end
+    def euro; self*RATES[:euros]; end
+    def ruble; self*RATES[:ruble]; end
     alias dollars dollar
     alias rubles ruble
     alias euros euro
     def in(x)
-        self/RATES[x.to_s]
+        self/RATES[x]
     end
 end
 class Numeric
     include Denezhka
+end
+class Array
+    include Denezhka
+    alias ddd dollar
+    def dollar; self.each {|x| x.ddd} ; end
+
 end
 puts 5.dollars
 puts 5.dollars.in(:euros)
